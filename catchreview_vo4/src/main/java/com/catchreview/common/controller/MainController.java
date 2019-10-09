68589339314032c64d6c3d59b57b992a48ef2cee
kjuiop <copying_y@naver.com>
2019-10-09 21:29:37 +0900

    환경설정 및 화면연결

---
+++ catchreview_vo4/src/main/java/com/catchreview/common/controller/MainController.java
@@ -0,0 +1,35 @@
+package com.catchreview.common.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
+
+@RequiredArgsConstructor
+@Controller
+@Log
+public class MainController {
+	
+//	private S3Uploader s3Uploader;
+	
+	@GetMapping("/")
+	public String index() {
+		log.info("index");
+		return "index";
+	}
+
+	@GetMapping("/mypage")
+	public String mypage() {
+		log.info("mypage");
+
+		return "mypage/mypageMain";
+	}
+
+//	@PostMapping("/upload")
+//	@ResponseBody
+//	public String upload(@RequestParam("data") MultipartFile multipartFile) throws IOException {
+//		return s3Uploader.upload(multipartFile, "static");
+//	}
+
+}
